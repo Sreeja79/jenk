a48f3c3bc9adcdeae7b3d0d9692b072d15612376
EC2 Default User <ec2-user@ip-172-31-16-20.us-west-2.compute.internal>
2018-03-26 20:23:49 +0000

    initial commit

---
+++ retangulator.java
@@ -0,0 +1,14 @@
+public class Rectangulator
+{
+public static void main(String[]args)
+{
+int length=Integer.parseInt(args[0]);
+int width=Integer.parseInt(args[1]);
+Rectangle myRectangle=new Rectangle(length,width);
+String output=String.format("***\n\n\Length: %d\nWidth: %d\nArea: %d\nPerimeter: %d\n\n", myRectangle.length, myRectangle.width, myRectangle.getArea(), myRectangle.getPerimeter());
+
+System.out.println(output);
+}
+}
+
+
